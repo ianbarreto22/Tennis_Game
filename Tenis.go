7dd25cfc0b48b77bb9209b042a433c2633ac370f
Ian Barreto <ian.barreto.700@ufrn.edu.br>
2022-12-21 19:56:25 -0300

    Ajustes no Simulador de Tênis

--- Tenis.go
+++ Tenis.go
@@ -6,28 +6,24 @@ import (
 	"time"
 )
 
-type PlayerStatus int64
-
 const (
-	Waiting PlayerStatus = iota
-	Hitting
-)
-
-const (
+	//P Number of points to a player win the game.
 	P int = 4
 )
 
+// TennisMatch keeps the two players of the tennis game.
 type TennisMatch struct {
-	Player1     TennisPlayer
-	Player2     TennisPlayer
-	PointsToWin int
+	Player1 TennisPlayer
+	Player2 TennisPlayer
 }
 
+// TennisPlayer keeps the name and points of a player
 type TennisPlayer struct {
 	Name   string
 	Points int
 }
 
+// playerTwo simulates a shot of the first player, if it misses the shot, it will increase the opponent's points
 func playerOne(player TennisPlayer, match chan TennisMatch, end chan bool) {
 	for true {
 		m := <-match
@@ -39,6 +35,7 @@ func playerOne(player TennisPlayer, match chan TennisMatch, end chan bool) {
 
 		if play <= 3 {
 			fmt.Println(player.Name, "errou!")
+			fmt.Println("Ponto do jogador:", m.Player2.Name+".")
 			m.Player2.Points += 1
 			printResult(m)
 		} else {
@@ -54,6 +51,7 @@ func playerOne(player TennisPlayer, match chan TennisMatch, end chan bool) {
 	}
 }
 
+// playerTwo simulates a shot o the second player, if it misses the shot, it will increase the opponent's points
 func playerTwo(player TennisPlayer, match chan TennisMatch, end chan bool) {
 	for true {
 		m := <-match
@@ -65,6 +63,7 @@ func playerTwo(player TennisPlayer, match chan TennisMatch, end chan bool) {
 
 		if play <= 3 {
 			fmt.Println(player.Name, "errou!")
+			fmt.Println("Ponto do jogador:", m.Player1.Name+".")
 			m.Player1.Points += 1
 			printResult(m)
 		} else {
@@ -81,12 +80,22 @@ func playerTwo(player TennisPlayer, match chan TennisMatch, end chan bool) {
 	}
 }
 
+// printResult prints the result of a tennis match
 func printResult(m TennisMatch) {
+	fmt.Println()
+	fmt.Println("Placar:")
 	fmt.Println(m.Player1.Name, m.Player1.Points, "x", m.Player2.Points, m.Player2.Name)
+	fmt.Println()
 }
 
+// isFinished returns true if one of the players have won the game, false otherwise
 func isFinished(match TennisMatch) bool {
-	if match.Player1.Points == P || match.Player2.Points == P {
+	pointsPlayer1, pointsPlayer2 := match.Player1.Points, match.Player2.Points
+	if pointsPlayer1 >= P && pointsPlayer1 >= pointsPlayer2+2 {
+		fmt.Println(match.Player1.Name, "venceu a partida!")
+		return true
+	} else if pointsPlayer2 >= P && pointsPlayer2 >= pointsPlayer1+2 {
+		fmt.Println(match.Player2.Name, "venceu a partida!")
 		return true
 	} else {
 		return false
@@ -115,6 +124,11 @@ func main() {
 
 	end := make(chan bool)
 
+	fmt.Println("-------------------------------------------")
+	fmt.Println()
+	fmt.Println("Começou a partida!")
+	fmt.Println()
+
 	go playerOne(player1, match, end)
 	go playerTwo(player2, match, end)
 
@@ -122,4 +136,11 @@ func main() {
 	for !finished {
 		finished = <-end
 	}
+
+	fmt.Println()
+	fmt.Println("Final da partida!")
+	fmt.Println()
+	fmt.Println("-------------------------------------------")
+
+	close(match)
 }
